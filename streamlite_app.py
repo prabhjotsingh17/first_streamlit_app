b55815331b9f9dfed7540ef560c63562c8fe03e8
prabhjotsingh17 <47354361+prabhjotsingh17@users.noreply.github.com>
2023-06-20 21:36:47 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -2,9 +2,11 @@ import streamlit
 
 streamlit.title('My Partners New Healthy Diner') 
 
-streamlit.header('Breakfast Menue')
-streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Roket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Roket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
