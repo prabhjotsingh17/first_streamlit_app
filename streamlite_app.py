c3c9508f1abc7eef6f747d6d34d09f40fddffd4e
prabhjotsingh17 <47354361+prabhjotsingh17@users.noreply.github.com>
2023-06-20 18:11:24 +0530

    Update streamlite_app.py
    Fixed Typo

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Partners New Healthy Diner') 
+
+streamlit.header('Breakfast Menue')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Roket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
