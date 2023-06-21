3e5be7c5a221ee266648e658781cce8f9b442ef4
prabhjotsingh17 <47354361+prabhjotsingh17@users.noreply.github.com>
2023-06-21 10:23:16 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -13,7 +13,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.setindex('Fruit'))
 streamlit.dataframe(my_fruit_list)
 
 
