8b45c988df70acb2c12e01bb4474c9a67201dd0f
prabhjotsingh17 <47354361+prabhjotsingh17@users.noreply.github.com>
2023-06-24 17:21:59 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-#streamlit.title('My Partners New Healthy Diner') 
+streamlit.title('My Mom's New Healthy Diner') 
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
@@ -17,4 +17,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
